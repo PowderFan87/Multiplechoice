5eba63824011696f121a3e85e4c8ff1811f33f21
pahartma <pahartma@DESTGN0H400367.emea.lucent.com>
2013-10-12 21:00:01 +0200

    - adding other files for views

---
+++ src/App/Data/Answer.php
@@ -0,0 +1,16 @@
+<?php
+class App_Data_Answer extends App_Data_Base
+{
+    const   VIEW_CLASS = 'viewAnswer';
+    const   VIEW_PK    = 'UID';
+
+    protected function getEmpryarray() {
+        return array(
+            /*'strQuestion'           => '',
+            'lngCountshowed'        => 0,
+            'lngOpttime'            => 0,
+            'tbldifficulty_UID'     => NULL,
+            'tblbackenduser_UID'    => NULL*/
+        );
+    }
+}
---
+++ src/App/Data/Category.php
@@ -0,0 +1,16 @@
+<?php
+class App_Data_Category extends App_Data_Base
+{
+    const   VIEW_CLASS = 'viewCategory';
+    const   VIEW_PK    = 'UID';
+
+    protected function getEmpryarray() {
+        return array(
+            /*'strQuestion'           => '',
+            'lngCountshowed'        => 0,
+            'lngOpttime'            => 0,
+            'tbldifficulty_UID'     => NULL,
+            'tblbackenduser_UID'    => NULL*/
+        );
+    }
+}

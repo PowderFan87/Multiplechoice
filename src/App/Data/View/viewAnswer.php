a6cb5e00403738b3ce70a59b54b1f98f0a8354cc
pahartma <pahartma@DESTGN0H400367.emea.lucent.com>
2013-10-15 12:22:35 +0200

    - viewAnswer.php: Funktion getAllAnswersByQuestionUID erstellt

--- src/App/Data/View/viewAnswer.php
+++ src/App/Data/View/viewAnswer.php
@@ -31,4 +31,34 @@ FROM
             return NULL;
         }
     }
+
+public static function getAllAnswersByQuestionUID($lngUID, $blnObjects)
+{
+     $strARClass = 'App_Data_' . self::VIEW_ARCLASS;
+        $strQuery   = '
+SELECT
+    *
+FROM
+    ' . self::VIEW_NAME . '
+WHERE tblquestion_UID == ' . $lngUID;
+
+        try {
+            $arrData        = App_Factory_Resource::getResource()->read($strQuery, true);
+
+            if($blnObjects) {
+                $arrResponse    = array();
+
+                foreach($arrData as $arrRow) {
+                    $arrResponse[] = new $strARClass($arrRow);
+                }
+
+                return $arrResponse;
+            } else {
+                return $arrData;
+            }
+        } catch (Resource_Exception $e) {
+            return NULL;
+        }
+}
+
 }

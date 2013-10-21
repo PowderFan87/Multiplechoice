0176da894e7a237b601248c1e70348747ee7df6a
pahartma <pahartma@DESTGN0H400367.emea.lucent.com>
2013-10-22 01:20:33 +0200

    - Statistik

--- src/App/Data/View/viewSession.php
+++ src/App/Data/View/viewSession.php
@@ -43,12 +43,97 @@ WHERE
         $strARClass = 'App_Data_' . self::VIEW_ARCLASS;
         $strQuery   = '
 SELECT
-    tblsessions.UID, tblsessions.dtmStart, tblcategory.strName, tblsessions.lngPoints
+    tblcategory.strName, COUNT(tblsessions.UID) As "lngPoints"
 FROM
     ' . self::VIEW_NAME . ', tblcategory
 WHERE
     tblsessions.tblcategory_UID = tblcategory.UID
 GROUP BY tblcategory.strName';
+
+        try {
+            $arrData        = App_Factory_Resource::getResource()->read($strQuery, true);
+
+            if($blnObjects) {
+                $arrResponse    = array();
+
+                foreach($arrData as $arrRow) {
+                    $arrResponse[] = new $strARClass($arrRow);
+                }
+
+                return $arrResponse;
+            } else {
+                return $arrData;
+            }
+        } catch (Resource_Exception $e) {
+            return NULL;
+        }
+    }
+    
+    public static function getCountsessions($blnObjects = true) {
+        $strARClass = 'App_Data_' . self::VIEW_ARCLASS;
+        $strQuery   = '
+SELECT
+    COUNT(UID) As "CountSessions"
+FROM
+    ' . self::VIEW_NAME;
+
+        try {
+            $arrData        = App_Factory_Resource::getResource()->read($strQuery, true);
+
+            if($blnObjects) {
+                $arrResponse    = array();
+
+                foreach($arrData as $arrRow) {
+                    $arrResponse[] = new $strARClass($arrRow);
+                }
+
+                return $arrResponse;
+            } else {
+                return $arrData;
+            }
+        } catch (Resource_Exception $e) {
+            return NULL;
+        }
+    }
+    
+    public static function getCountsessionsbypoints($blnObjects = true) {
+        $strARClass = 'App_Data_' . self::VIEW_ARCLASS;
+        $strQuery   = '
+SELECT
+    COUNT(UID) As "CountSuccessful"
+FROM
+    ' . self::VIEW_NAME . '
+WHERE
+    lngPoints > "84"';
+
+        try {
+            $arrData        = App_Factory_Resource::getResource()->read($strQuery, true);
+
+            if($blnObjects) {
+                $arrResponse    = array();
+
+                foreach($arrData as $arrRow) {
+                    $arrResponse[] = new $strARClass($arrRow);
+                }
+
+                return $arrResponse;
+            } else {
+                return $arrData;
+            }
+        } catch (Resource_Exception $e) {
+            return NULL;
+        }
+    }
+    
+    public static function getSessionsbydate($blnObjects = true) {
+        $strARClass = 'App_Data_' . self::VIEW_ARCLASS;
+        $strQuery   = '
+SELECT
+    COUNT(UID) As "Anzahl", dtmStart As "Datum"
+FROM
+    ' . self::VIEW_NAME . '
+GROUP BY
+    dtmStart';
 
         try {
             $arrData        = App_Factory_Resource::getResource()->read($strQuery, true);
